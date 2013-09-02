6fd7a056106ce2567c59852939624a3bad7a1302
Evgeniy Kuzmin <jekccs@gmail.com>
2013-09-02 16:54:25 +0300

    anatations added

--- ControllerTest.php
+++ ControllerTest.php
@@ -4,38 +4,49 @@ namespace LaMelle\PhpUnit\Tester;
 
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * PhpUnit Extension for Symfony2 controllers unit tests 
+ *
+ * @author Evgen Kuzmin <jekccs@gmail.com>
+ */
 class ControllerTest extends ServiceTest
 {
-  protected $request;
-  protected $controller;
-  protected $twig;
+    protected $request;
+    protected $controller;
+    protected $twig;
 
-  function __construct($controller, $isTwigEmulation = false)
-  {
-          parent::__construct();
-          
-          $this->request = new Request();
-          $this->container->enterScope('request');
-          $this->container->set('request', $this->request, 'request');
-          
-          // Mock templating
-          if($isTwigEmulation)
-          {
-            $this->twig = $this->getMockBuilder('Symfony\Bundle\FrameworkBundle\Templating\Engine')
-                               ->setMethods(array('render'))
-                               ->getMock();
-            
-            $this->twig->expects($this->any())
-                      ->method('render')
-                      ->will($this->returnValue('success'))
-                        ;
-          }  
-          // real render 
-          else   
-            $this->twig = $this->container->get('twig'); 
-          
-          $this->controller = new $controller;
-          $this->controller->setContainer($this->container);
-  }
-  
+	/**
+	 * Initializes a new instance of the ControllerTest class.
+	 *
+	 * @param string $controller path to controller to test
+	 * @param bool   $isTwigEmulation switcher for twig mock emulation or real render
+	 */    
+    function __construct($controller, $isTwigEmulation = false)
+    {
+            parent::__construct();
+
+            $this->request = new Request();
+            $this->container->enterScope('request');
+            $this->container->set('request', $this->request, 'request');
+
+            // Mock templating
+            if($isTwigEmulation)
+            {
+                $this->twig = $this->getMockBuilder('Symfony\Bundle\FrameworkBundle\Templating\Engine')
+                                   ->setMethods(array('render'))
+                                   ->getMock();
+
+                $this->twig->expects($this->any())
+                           ->method('render')
+                           ->will($this->returnValue('success'))
+                            ;
+            }  
+            // real twig render 
+            else   
+                $this->twig = $this->container->get('twig'); 
+
+            $this->controller = new $controller;
+            $this->controller->setContainer($this->container);
+    }
+   
 }
--- ServiceTest.php
+++ ServiceTest.php
@@ -3,16 +3,27 @@ namespace LaMelle\PhpUnit\Tester;
 
 require_once __DIR__.'/../../../../../../app/AppKernel.php';
 
+/**
+ * PhpUnit Extension for Symfony2 services unit tests 
+ *
+ * @author Evgen Kuzmin <jekccs@gmail.com>
+ */
 class ServiceTest extends \PHPUnit_Framework_TestCase
 {
   protected static $kernel;
   protected $container;
 
-  function __construct()
-  {
-          $this->init();
-          parent::__construct();
-  }
+	/**
+	 * Initializes a new instance of the ServiceTest class.
+	 *
+	 * @param string      $key   unique name for the lock
+	 * @param string|null $path  put lock file in this folder, use null for system temp dir
+	 */  
+    function __construct()
+    {
+            $this->init();
+            parent::__construct();
+    }
   
 	/**
 	 * @return void
