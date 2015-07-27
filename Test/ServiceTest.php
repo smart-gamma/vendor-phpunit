81cbd8c9e227347e0f32e525cf00355f6a7a9848
Dmytro Monastyreckyi <dmytro@skeegleapp.com>
2015-07-27 23:17:17 +0300

    refactored ServiceTest

--- Test/ServiceTest.php
+++ Test/ServiceTest.php
@@ -13,18 +13,21 @@ use Symfony\Component\HttpFoundation\Request;
 abstract class ServiceTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * Flag at the phpunit cli to switch off mock emulation and work with real classes
+     *
+     * Usage: phpunit -d noMock ...
+     */
+    const DISABLE_EMULATION_CLI_ARG = 'nomock';
+
+    const REQUEST_SCOPE = 'request';
+
+    /**
      * Selector to use real twig and inhouse classes or mock them
      *
      * @var bool
      */
     protected $isMockEmulation = true;
 
-    /**
-     * Flag at the phpunit cli to switch off mock emulation and work with real classes
-     * Usage: phpunit -d noMock ... 
-     */
-    const DISABLE_EMULATION_CLI_ARG = 'nomock';
-    
     /**
      * List of mocking repositories when $isMockEmulation = true;
      * 
@@ -34,24 +37,28 @@ abstract class ServiceTest extends \PHPUnit_Framework_TestCase
     
     /**
      * App kernel
-     * @var AppKernel $kernel
+     *
+     * @var \AppKernel $kernel
      */
     protected static $kernel;
 
     /**
      * DI container
-     * @var Symfony\Component\DependencyInjection\ContainerInterface $container
+     *
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface $container
      */
     protected $container;
 
     /**
      * Target class name for the instance creation
+     *
      * @var mixed $instance
      */
     protected $targetClassName;
     
     /**
      * Target test service
+     *
      * @var mixed $instance
      */
     protected $instance;
@@ -65,27 +72,31 @@ abstract class ServiceTest extends \PHPUnit_Framework_TestCase
     
     /**
      * Initializes a new instance of the ServiceTest class.
+     *
+     * @param null   $name
+     * @param array  $data
+     * @param string $dataName
      */
-    public function __construct()
+    public function __construct($name = null, array $data = array(), $dataName = '')
     {
         $this->processEmulationMode();
-                
-        //Kernel/container startup
         $this->init();
-        parent::__construct();
 
-        //Call container independent service
-        if (!empty($this->targetClassName) && !$this->isConstructContainer)
-            $this->instance = new $this->targetClassName;
+        if (!empty($this->targetClassName)) {
+            if (!$this->isConstructContainer) {
+                //Call container independent service
+                $this->instance = new $this->targetClassName;
+            } else {
+                //Call container dependent service
+                $this->instance = new $this->targetClassName($this->container);
+            }
+        }
 
-        //Call container dependent service
-        if (!empty($this->targetClassName) && $this->isConstructContainer)
-            $this->instance = new $this->targetClassName($this->container);
+        parent::__construct($name, $data, $dataName);
     }
 
     /**
      * Boot Kernel and container init
-     * @return void
      */
     protected function init()
     {
@@ -93,8 +104,15 @@ abstract class ServiceTest extends \PHPUnit_Framework_TestCase
         self::$kernel->boot();
 
         $this->container = self::$kernel->getContainer();
-        $this->container->enterScope('request');
-        $this->container->set('request', new Request(), 'request');
+    }
+
+    /**
+     * @param string $scope
+     */
+    protected function setScope($scope = self::REQUEST_SCOPE)
+    {
+        $this->container->enterScope($scope);
+        $this->container->set('request', new Request(), $scope);
     }
 
     /**
@@ -102,72 +120,87 @@ abstract class ServiceTest extends \PHPUnit_Framework_TestCase
      * This method is called before a test is executed.
      */
     protected function setUp()
-    {        
-        //ORM emulation
-        if ($this->isMockEmulation) {
-            //pass all dependent emulated repositories to EntityManager 
-            $mockedRepositories = array();
+    {
+        if (!$this->isMockEmulation) {
+            $this->instance->setEm($this->container->get("doctrine.orm.entity_manager"));
+
+            return;
+        }
+
+        if ($this->emulatedRepositoriesList) {
+            //pass all dependent emulated repositories to EntityManager
             foreach($this->emulatedRepositoriesList as $emulatedRepository) {
                 $mock = new $emulatedRepository();
                 $mockedRepositories[$mock->getRepositoryName()] = $mock->getRepositoryMock();
             }
-            if(sizeof($this->emulatedRepositoriesList) > 0) {
-                $this->instance->setEm($this->getEntityManagerMock($mockedRepositories));
-            } 
-            // none emulated reposirories created yet, so lets use real ones for classes with entity manger passed
-            elseif(method_exists($this->instance,'setEm')) {                 
-                $this->instance->setEm($this->container->get("doctrine.orm.entity_manager"));
-            }  
+
+            $this->instance->setEm($this->getEntityManagerMock($mockedRepositories));
+
+            return;
         }
-        // Real ORM
-        elseif(method_exists($this->instance,'setEm')) {
+
+        // none emulated reposirories created yet, so lets use real ones for classes with entity manger passed
+        if(method_exists($this->instance,'setEm')) {
             $this->instance->setEm($this->container->get("doctrine.orm.entity_manager"));
-        }    
+        }
     }
     
     /**
      * Get mock of Entity manager
-     * @param  array  $mockedRepositories set with Repository behavior 
-     * @return \Doctrine\ORM\Mock_EntityManager
+     *
+     * @param array $mockedRepositories
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
      */
     private function getEntityManagerMock($mockedRepositories = array())
     {
-        $mockEntityManager = $this->getMock('\Doctrine\ORM\EntityManager', array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
+        $methods = array(
+            'getRepository',
+            'getClassMetadata',
+            'persist',
+            'flush',
+        );
+
+        $mockEntityManager = $this->getMock('\Doctrine\ORM\EntityManager', $methods, array(), '', false);
 
         $mockEntityManager->expects($this->any())
-                ->method('getClassMetadata')
-                ->will($this->returnValue((object) array('name' => 'aClass')));
+            ->method('getClassMetadata')
+            ->will($this->returnValue((object) array('name' => 'aClass')))
+        ;
 
         $mockEntityManager->expects($this->any())
-                ->method('persist')
-                ->will($this->returnValue(null));
+            ->method('persist')
+            ->will($this->returnValue(null))
+        ;
 
         $mockEntityManager->expects($this->any())
-                ->method('flush')
-                ->will($this->returnValue(null));
-        
+            ->method('flush')
+            ->will($this->returnValue(null))
+        ;
+
         $mockEntityManager->expects($this->any())
-                        ->method('getRepository')
-                        ->with($this->anything())
-                        ->will($this->returnCallback(function($repositoryName) use ($mockedRepositories) { 
-                                                            return $mockedRepositories[$repositoryName];
-                                                     }
-                                                     )
-                              );
+            ->method('getRepository')
+            ->with($this->anything())
+            ->will($this->returnCallback(function($repositoryName) use ($mockedRepositories) {
+                return $mockedRepositories[$repositoryName];
+            }))
+        ;
         
         return $mockEntityManager;
     }
     
     /**
      * Parse phpunit cli for disabling mock emulation mode, that is enabled by default
-     * @return void 
+     *
+     * @return void
      */
-    private function processEmulationMode() {
+    private function processEmulationMode()
+    {
         global $argv;
         
         foreach($argv as $arg) {
             if($arg == self::DISABLE_EMULATION_CLI_ARG) {
-                $this->isMockEmulation =  false;   
+                $this->isMockEmulation = false;
             }
         } 
     }
